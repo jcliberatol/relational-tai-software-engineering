aff5f265bc0f5b1a1161469cccfdeaa8e417e025
Camilo <Camilo@CAMILO>
2012-04-02 12:48:40 -0500

    Cronometro =D

--- PrimaryTest/js/PrimaryTest.js
+++ PrimaryTest/js/PrimaryTest.js
@@ -2,13 +2,41 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-function generarNumero(){
-    return numero=Math.round(Math.random()*10) + 1;    
+var numero1;
+var numero2;
+var respuesta;
+var CronoID = null;
+var CronoEjecutandose = false;
+var segundos=0;
+
+function InicializarCrono () {
+    segundos = 0;
 }
+
+function MostrarCrono() {
+    var ValorCrono = segundos;
+    document.getElementById('time').value = ValorCrono;
+    CronoID = setTimeout("MostrarCrono()", 1000);
+    segundos++;
+
+}
+
+function IniciarCrono () {
+    DetenerCrono();
+    InicializarCrono();
+    MostrarCrono();
+}
+
+function setearNumeros(){
+    numero1 = Math.round(Math.random()*10) + 1;
+    numero2 = Math.round(Math.random()*10) + 1;
+    repuesta = numero1 * numero2;
+    var pregunta = numero1 + "   x   " + numero2;
+    document.getElementById('question').value =  pregunta;
+    document.getElementById('response').value = "";
+}
+
 function validarPregunta(tipo){
-    var numero1 = document.getElementById('number1').value;
-    var numero2 = document.getElementById('number2').value;
-    var respuesta = document.getElementById('response').value;
     var validado;
     
     if(tipo==0){
@@ -26,9 +54,8 @@ function validarPregunta(tipo){
     if((tipo != 1) && (tipo != 2) && (tipo != 3) && (tipo != 0)){
         validado = 'no se puede validar';
     }
-    document.getElementById('response').value = '';
-    document.getElementById('number1').value = generarNumero();
-    document.getElementById('number2').value = generarNumero();
+    setearNumeros();
+    segundos=0;
     return validado;
 }
 function enterPresionado(numero, e){
